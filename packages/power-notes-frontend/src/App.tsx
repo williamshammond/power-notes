10a59072cb3a050b0eb638e5237d1d7aa6798492
Will Hammond <wsh2117@columbia.edu>
2024-02-07 21:23:56 -0500

    Configure auth0, add LoginButton, LogoutButton, UserProfile components

--- packages/power-notes-frontend/src/App.tsx
+++ packages/power-notes-frontend/src/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import "./App.css";
 import { HomePage } from "./home-page/HomePage";
-import { PersistentLeftMenu } from "./navigation-components/PersistentLeftMenu";
 
 function App() {
-  return (
-    <React.Fragment>
-      <PersistentLeftMenu />
-      <HomePage />
-    </React.Fragment>
-  );
+  return <HomePage />;
 }
 
 export default App;
--- packages/power-notes-frontend/src/home-page/HomePage.tsx
+++ packages/power-notes-frontend/src/home-page/HomePage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { LoginButton } from "../auth-components/LoginButton";
+import LogoutButton from "../auth-components/LogoutButton";
+import { UserProfile } from "../auth-components/UserProfile";
+import { PersistentLeftMenu } from "../navigation-components/PersistentLeftMenu";
 
 export function HomePage() {
   return (
     <React.Fragment>
       <h1>Home Page</h1>
+      <PersistentLeftMenu />
+      <LoginButton />
+      <LogoutButton />
+      <UserProfile />
       <p>
         This is the home page for the Power Notes application. It is a
         placeholder for the actual home page.
