eb60d32ddde749d780990ccfad3e33b039e8cf34
Will Hammond <wsh2117@columbia.edu>
2024-02-11 17:57:40 -0500

    Scaffold ErrorPage, NoteHome

--- packages/power-notes-frontend/src/App.tsx
+++ packages/power-notes-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./home-page/HomePage";
+import { ErrorPage } from "./error-components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
