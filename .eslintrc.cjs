99f24490e32243781ab445d6d93560b80a1c3d51
Will Hammond <wsh2117@columbia.edu>
2024-02-13 22:55:44 -0500

    Delete power-notes-frontend .eslintrc.cjs, add more global eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,26 @@
 /* eslint-env node */
 module.exports = {
     extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:@typescript-eslint/recommended',
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['react', 'react-hooks', '@typescript-eslint'],
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react", "react-hooks", "@typescript-eslint"],
     root: true,
     settings: {
         react: {
-            version: 'detect',
+            version: "detect",
         },
     },
-}
+    rules: {
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            { argsIgnorePattern: "^_" },
+        ],
+        "react/jsx-fragments": ["error", "element"],
+        "react/react-in-jsx-scope": "off",
+    },
+};
