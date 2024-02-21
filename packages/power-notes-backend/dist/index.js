ad6b2803898681d6fb031aae35d434d7b566f0c8
Will Hammond <wsh2117@columbia.edu>
2024-02-20 23:22:10 -0500

    Add concurrently, ts-node dependencies backend

--- packages/power-notes-backend/dist/index.js
+++ packages/power-notes-backend/dist/index.js
@@ -5,13 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv = require("dotenv");
-dotenv.config({ path: ".env.local" });
-dotenv.config();
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: ".env.local" });
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
 app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server test change");
+    res.send("Hello world!!!");
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
