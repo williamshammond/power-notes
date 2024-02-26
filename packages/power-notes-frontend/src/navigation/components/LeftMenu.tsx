e2ac1864f261b61d67dcca3318b1606bbb9d7c5b
Will Hammond <wsh2117@columbia.edu>
2024-02-25 20:54:12 -0500

    Add nested folders and items style logic

--- packages/power-notes-frontend/src/navigation/components/LeftMenu.tsx
+++ packages/power-notes-frontend/src/navigation/components/LeftMenu.tsx
@@ -1,11 +1,10 @@
 import { ExpandMore } from "@mui/icons-material";
 import {
     Accordion,
-    AccordionActions,
     AccordionDetails,
     AccordionSummary,
     Box,
-    Button,
+    Divider,
 } from "@mui/material";
 import React from "react";
 
@@ -47,25 +46,81 @@ export const LeftMenu = function LeftMenu() {
     return (
         <Box>
             {folders != null
-                ? folders.folders.map((folder) => displayFolder(folder))
+                ? folders.folders.map((folder) => displayFolder(folder, 0))
                 : "Nothing yet"}
         </Box>
     );
 };
 
-function displayFolder(folder: Folder): JSX.Element {
+function displayFolder(folder: Folder, layer: number): JSX.Element {
     return (
-        <Accordion key={folder.id}>
-            <AccordionSummary
-                expandIcon={<ExpandMore />}
-                aria-controls="panel3-content"
-                id="panel3-header"
-            >
+        <Accordion
+            key={folder.id}
+            sx={{ backgroundColor: calculateBackground(layer) }}
+            defaultExpanded={false}
+        >
+            <AccordionSummary expandIcon={<ExpandMore />} id="panel3-header">
                 {folder.name}
             </AccordionSummary>
-            <AccordionDetails>
-                {folder.subfolders.map((subfolder) => displayFolder(subfolder))}
+            <AccordionDetails sx={{ padding: "0" }}>
+                {folder.subfolders.length === 0 &&
+                    folder.notes.length === 0 &&
+                    folder.todos.length === 0 &&
+                    folder.journals.length === 0 && (
+                        <Box
+                            sx={{
+                                backgroundColor: calculateBackground(layer + 1),
+                                padding: "2 0 2 0",
+                            }}
+                        >
+                            No content in folder
+                        </Box>
+                    )}
+                {folder.subfolders.map((subfolder) =>
+                    displayFolder(subfolder, layer + 1)
+                )}
+                {folder.notes.length > 0 && <Divider>Notes</Divider>}
+                {folder.notes.map((note) => (
+                    <React.Fragment key={note.id}>
+                        <Box
+                            key={note.id}
+                            sx={{
+                                height: "30px",
+                                padding: "2 0 2 0",
+                                borderTop: "1px solid gray",
+                                borderBottom: "1px solid gray",
+                            }}
+                        >
+                            {note.name}
+                        </Box>
+                    </React.Fragment>
+                ))}
+                {folder.todos.length > 0 && <Divider>Todos</Divider>}
+                {folder.todos.map((todo) => (
+                    <Box key={todo.id} sx={{ padding: "2 0 2 0" }}>
+                        {todo.name}
+                    </Box>
+                ))}
+                {folder.journals.length > 0 && <Divider>Journal</Divider>}
+                {folder.journals.map((journal) => (
+                    <Box key={journal.id} sx={{ padding: "2 0 2 0" }}>
+                        {journal.name}
+                    </Box>
+                ))}
             </AccordionDetails>
         </Accordion>
     );
+}
+function calculateBackground(layer: number): string {
+    const baseHue = 0;
+    const baseSaturation = 0;
+    const baseLightness = 0;
+
+    const lightnessIncrement = 4;
+    const adjustedLightness = baseLightness + layer * lightnessIncrement;
+
+    const finalLightness = Math.min(adjustedLightness, 100);
+    const finalColor = `hsl(${baseHue}, ${baseSaturation}%, ${finalLightness}%)`;
+
+    return finalColor;
 }
